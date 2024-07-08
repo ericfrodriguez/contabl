86a7dbc7623445b26013ed1cdaf7f8084adc7fbc
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-07-08 01:00:54 -0300

    forcing home dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { db } from "@/server/db";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const posts = await db.query.posts.findMany();
   console.log(posts);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello world
+      {
+        posts[0]?.name
+      }
     </main>
   );
 }
